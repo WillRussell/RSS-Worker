0115b508a50b9cd8e6c043657add97713522108f
willrussell <wrussell35@gmail.com>
2022-09-02 17:40:46 -0700

    Fix occasional download error
    * adds buffer opts to the shell command
    * resolves: "stdout: maxBuffer length exceeded"

--- utilities/downloadVideo.js
+++ utilities/downloadVideo.js
@@ -7,7 +7,7 @@ module.exports.downloadVideo = async (url) => {
   const shellCommand = `yt-dlp -o './downloads/%(title)s.%(ext)s' -x --audio-format mp3 ${url}`;
 
   const downloadPromise = new Promise((resolve, reject) => {
-    exec(shellCommand, (err, stdout) => {
+    exec(shellCommand, { maxBuffer: 1024 * 500 }, (err, stdout) => {
       if (err) return reject(err);
       console.log(stdout);
       resolve(stdout);
