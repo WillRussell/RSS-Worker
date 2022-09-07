96835aec920c3f51c2871cecfe75417578d85e6a
willrussell <wrussell35@gmail.com>
2022-09-07 12:07:57 -0700

    Get RSS xml uploads working
    * takes the .xml file at the project root and
      uploads it to the s3 bucket.
    
    * rss feed is publicly exposed with a working url
      that can be used in any podcast client
    
    * automated transformation of youtube video to
      podcast is working!

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { getUrl } = require("./utilities/getUrl");
 const { downloadVideo } = require("./utilities/downloadVideo");
 const { uploadPodcast } = require("./utilities/uploadPodcast");
+const { updateRss } = require("./utilities/updateRss");
 
 async function run() {
   const url = await getUrl();
@@ -9,9 +10,9 @@ async function run() {
   
   const uploadInfo = await uploadPodcast();
 
-  // update the rss xml
+  // generate xml dynamically
 
-  console.log("FINISHED");
+  const rssInfo = await updateRss();
 }
 
 run();
---
+++ utilities/updateRss.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const fs = require("fs");
+
+const AWS = require("aws-sdk");
+const bucketName = process.env["BUCKET_NAME"];
+const accessKeyId = process.env["ACCESS_KEY_ID"];
+const secretAccessKey = process.env["SECRET_ACCESS_KEY_ID"];
+
+const s3 = new AWS.S3({
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccessKey,
+});
+
+module.exports.updateRss = async () => {
+  console.log("Updating RSS File...");
+
+  const fileName = "rss.xml";
+  const filePath = `./${fileName}`;
+  const file = fs.readFileSync(filePath);
+
+  const params = {
+    Key: fileName,
+    Bucket: bucketName,
+    Body: file,
+    ContentType: "text/xml",
+    ACL: "public-read",
+  };
+
+  const updatePromise = new Promise((resolve, reject) => {
+    s3.upload(params, (err, data) => {
+      if (err) return reject(err);
+      console.log(data);
+      resolve(data);
+    });
+  });
+
+  return await updatePromise;
+};
