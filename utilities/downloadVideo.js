4df857ae028618575cf248362610c1eeac7233aa
willrussell <wrussell35@gmail.com>
2022-09-12 09:45:26 -0700

    Increase max buffer on shell downloads
    * downloads were still occasionally getting hung
      up on CHILD_PROCESS_STDIO_MAXBUFFER error

--- utilities/downloadVideo.js
+++ utilities/downloadVideo.js
@@ -7,7 +7,7 @@ module.exports.downloadVideo = async (url) => {
   const shellCommand = `yt-dlp -o './downloads/%(title)s.%(ext)s' -x --audio-format mp3 ${url}`;
 
   const downloadPromise = new Promise((resolve, reject) => {
-    exec(shellCommand, { maxBuffer: 1024 * 500 }, (err, stdout) => {
+    exec(shellCommand, { maxBuffer: 5 * 1024 * 1024 }, (err, stdout) => {
       if (err) return reject(err);
       console.log(stdout);
       resolve(stdout);
