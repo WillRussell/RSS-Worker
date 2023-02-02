358ccf2a90da996874f16af9d540a1702d3b19f8
willrussell <wrussell35@gmail.com>
2023-02-01 18:30:28 -0800

    Enable Youtube url to be passed in as argument
    * addresses the latest change to youtube ui which
      breaks the getUrl() automation task

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const { getUrl } = require("./utilities/getUrl");
+const { getMrLiveUrl } = require("./utilities/getMrLive");
 const { downloadVideo } = require("./utilities/downloadVideo");
 const { uploadPodcast } = require("./utilities/uploadPodcast");
 const { updateRss } = require("./utilities/updateRss");
 const { generateXml } = require("./utilities/generateXml");
 
 async function run() {
-  const url = await getUrl();
+  const args = process.argv.slice(2);
+  const url = args[0];
+  // const url = await getMrLiveUrl();
+
   await downloadVideo(url);
   await uploadPodcast();
   await generateXml();
   await updateRss();
-  
+
   console.log("Task Complete: Youtube video transformed to Podcast");
 }
 
