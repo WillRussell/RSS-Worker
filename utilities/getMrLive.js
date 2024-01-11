1b067754d6b105fb57af2b949b33b87e03395237
willrussell <wrussell35@gmail.com>
2024-01-11 16:15:21 -0500

    Refactor with updated npm dep

--- utilities/getMrLive.js
+++ utilities/getMrLive.js
@@ -1,47 +1,33 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const { Client } = require("youtubei");
-const { get, pick } = require("lodash");
+const { Client } = require('youtubei');
+const { get, pick } = require('lodash');
 
-const channelName = process.env["CHANNEL_NAME"];
+const channelName = process.env['CHANNEL_NAME'];
 
 const youtube = new Client();
 
-const isUrl = (str) => str.toLowerCase().includes("https://");
+const isUrl = (str) => str.toLowerCase().includes('https://');
 
 module.exports.getMrLiveUrl = async () => {
-
   /* Step 1: Find the Youtube channel & get latest uploads */
-  const channel = await youtube.findOne(channelName, { type: "channel" });
-  await channel.videos.next(); // most recent 30 videos
+  const channel = await youtube.findOne(channelName, { type: 'channel' });
+  await channel.live.next(); // most recent 30 live videos
 
-  /* Step 2: Create a list of only live broadcasts */
-  const liveShowList = channel.videos.items.filter((item) =>
-    item.title.toLowerCase().includes("mr live")
-  );
+  /* Step 2: Get video ID of the most recent live broadcast  */
+  const latestLiveShow = channel.live.items.shift();
+  const liveShowObj = pick(latestLiveShow, ['id', 'title', 'duration']);
 
-  // liveShowList is broken since Youtube changed its UI and removed live streams from the "Videos" tab.
-  // The YoutubeI api has no way of accessing the new "Live" tab where old streams are stored.
-  // Github issue:
-  // https://github.com/SuspiciousLookingOwl/youtubei/issues/72#issuecomment-1403395409
-
-  console.log("LIVE SHOW LIST");
-  console.log(liveShowList);
-
-  /* Step 3: Get video ID of the most recent live broadcast  */
-  const latestLiveShow = liveShowList.shift();
-  const liveShowObj = pick(latestLiveShow, ["id", "title", "duration"]);
-
-  /* Step 4: Fetch the video description  */
+  /* Step 3: Fetch the video description  */
   const video = await youtube.getVideo(liveShowObj.id);
-  const videoDescription = get(video, "description");
+  const videoDescription = get(video, 'description');
 
-  /* Step 5: Parse the target URL from the description */
-  const descriptionList = videoDescription.split("\n");
+  /* Step 4: Parse the target URL from the description */
+  const descriptionList = videoDescription.split('\n');
   const textRow = descriptionList.find((str) =>
-    str.toLowerCase().includes("fun half")
+    str.toLowerCase().includes('fun half')
   );
-  const textCollection = textRow.split(" ");
+  const textCollection = textRow.split(' ');
   const endString = textCollection.pop();
 
   // If text row contains multiple URLs, it's preferable to use the last one
