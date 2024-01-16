6a4f518212a8e1cc534600dfbb0dc7e607c24758
willrussell <wrussell35@gmail.com>
2024-01-15 19:57:56 -0500

    Refactor string templates

--- utilities/downloadVideoDescription.js
+++ utilities/downloadVideoDescription.js
@@ -10,12 +10,14 @@ module.exports.downloadVideoDescription = async (url) => {
   // %(description)j will render the description as a json string
   // %(description)s will render the description as a string
 
-  const shellCommand = `yt-dlp --skip-download --print '{"id":"%(id)s", "description":"%(description)j", "upload_date":"%(upload_date>%Y-%m-%d)s", "duration":"%(duration_string)s"}' ${url}`;
+  const dlTemplate = `{"id":%(id)j, "upload_date":%(upload_date>%Y-%m-%d)j, "duration":%(duration_string)j, "description":%(description)j}`;
+
+  const shellCommand = `yt-dlp --skip-download --print '${dlTemplate}' ${url}`;
   const downloadPromise = new Promise((resolve, reject) => {
     exec(shellCommand, { maxBuffer: 5 * 1024 * 1024 }, (err, stdout) => {
       if (err) return reject(err);
-      console.log(stdout);
-      resolve(stdout);
+      const result = JSON.parse(stdout);
+      resolve(result);
     });
   });
 
