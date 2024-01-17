d7096c48a2444821eccf7666a8abfed3952deade
willrussell <wrussell35@gmail.com>
2024-01-16 19:43:44 -0500

    Improve video metadata logging

--- utilities/downloadVideoDescription.js
+++ utilities/downloadVideoDescription.js
@@ -1,7 +1,13 @@
 const { exec } = require('child_process');
+const chalk = require("chalk");
+
+function logInfo(message, value) {
+  console.log(chalk.blackBright(message + ': ') + chalk.white(value));
+}
 
 module.exports.downloadVideoDescription = async (url) => {
-  console.log('Getting video description...');
+
+  console.log(chalk.bold.blueBright("\nFetching video meta data...\n"));
 
   // --skip-download skips downloading the video
   // --print prints the video metadata
@@ -9,14 +15,22 @@ module.exports.downloadVideoDescription = async (url) => {
 
   // %(description)j will render the description as a json string
   // %(description)s will render the description as a string
-
-  const dlTemplate = `{"id":%(id)j, "upload_date":%(upload_date>%Y-%m-%d)j, "duration":%(duration_string)j, "description":%(description)j}`;
+  
+  const dlTemplate = `{"id":%(id)j, "upload_date":%(upload_date>%m-%d-%Y)j, "duration":%(duration_string)j, "title":%(title)j, "uploader":%(uploader)j}`;
 
   const shellCommand = `yt-dlp --skip-download --print '${dlTemplate}' ${url}`;
   const downloadPromise = new Promise((resolve, reject) => {
     exec(shellCommand, { maxBuffer: 5 * 1024 * 1024 }, (err, stdout) => {
       if (err) return reject(err);
       const result = JSON.parse(stdout);
+      
+      const { id, title, upload_date, duration, uploader } = result;
+      logInfo('Channel Name', uploader);
+      logInfo('Video Title', title);
+      logInfo('Video ID', id);
+      logInfo('Uploaded', upload_date);
+      logInfo('Duration', duration);
+
       resolve(result);
     });
   });
